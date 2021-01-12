9cb6cb233e5d432df3e303c2f5aebc7ca2d6a302
yoshimi.tanaka <ymym4432@au.com>
2021-01-12 14:05:46 +0000

    課題更新2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+// ユーザ機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
+
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');

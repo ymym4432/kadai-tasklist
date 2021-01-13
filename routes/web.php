24be8dc0a82cbf6c58bada873adef63ff1abfca1
yoshimi.tanaka <ymym4432@au.com>
2021-01-13 04:18:57 +0000

    更新課題4

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,8 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 // ユーザ機能
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('tasks', 'TasksController');
 });
 
 Route::get('/', 'TasksController@index');
 
-Route::resource('tasks', 'TasksController');
